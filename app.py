4ca5a12babc98ad6ce746e0d7c177067b7e181e5
nsawtelle <nicolelsawtelle@gmail.com>
2025-07-28 19:12:12 +0000

    database connection2

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def hello_world():
     return 'Hello World from Nicole Sawtelle in 3308'
 
 @app.route('/db_test')
-def db_test():
+def testing():
     conn = psycopg2.connect("postgresql://db_3308_flask_db_user:80XZPI6NZDC7TVZa2OifsiV6yTm8cEAR@dpg-d23rademcj7s739m9jag-a/db_3308_flask_db")
     conn.close()
-    return "Database connection successful"
+    return "Database Connection Successful"
